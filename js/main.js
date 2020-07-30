a3956b8d8b7c89243d0138d9269bb58d894daff5
HYivisir <843702140@qq.com>
2020-07-30 08:56:34 +0800

    新增页面搭建

--- js/main.js
+++ js/main.js
@@ -23,12 +23,20 @@ window.onload = function(){
     optTabs[0].onclick = function(){
         tab(this);
         optPanes[1].style.display = 'none';
+        optPanes[2].style.display = 'none';
         optPanes[0].style.display = 'flex'
     }
     optTabs[1].onclick = function(){
         tab(this);
         optPanes[0].style.display = 'none';
+        optPanes[2].style.display = 'none';
         optPanes[1].style.display = 'flex'
+    }
+    optTabs[2].onclick = function(){
+        tab(this);
+        optPanes[0].style.display = 'none';
+        optPanes[1].style.display = 'none';
+        optPanes[2].style.display = 'flex'
     }
 
 }
