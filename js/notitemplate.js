ea626f408c9d835ee63f61a57cac30c2db9b47e8
LShang233 <3014899575@qq.com>
2020-08-02 16:49:35 +0800

    报名信息：修改svg鼠标显示

--- js/notitemplate.js
+++ js/notitemplate.js
@@ -186,7 +186,6 @@ window.onload = function () {
         }
     }
 
-
     /* 添加删除事件 */
     function addDeleteClick() {
         let isDe = document.getElementById("noti-isde");
@@ -223,6 +222,5 @@ window.onload = function () {
             isDe.classList.add("hide");
         }
     }
-
 }
 
